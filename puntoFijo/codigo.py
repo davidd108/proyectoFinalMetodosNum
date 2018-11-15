ba73574ac5795ef7dc85138d6622a98b4b636548
davidd108 <41306199+davidd108@users.noreply.github.com>
2018-11-15 15:12:38 -0600

    Update codigo.py

--- puntoFijo/codigo.py
+++ puntoFijo/codigo.py
@@ -1,1 +1,14 @@
-
+def xnew(x):
+    return (2*x**2 + 3) / 5
+x0 = 0
+x1 = 0
+itera = 0
+for i in range(100):
+    x1 = xnew(x0)
+    if abs(x1 - x0) < 0.000001:
+        break
+    x0 = x1
+    itera += 1
+    
+print("La raiz es %.5f"%x1)
+print("Usando %d iteraciones"%itera)
