4d416bea2808e8185ededc5551254152b7e25838
davidd108 <41306199+davidd108@users.noreply.github.com>
2018-11-16 10:44:32 -0600

    Update codigo.py

--- interpolacionPolinomial/codigo.py
+++ interpolacionPolinomial/codigo.py
@@ -1,162 +1,141 @@
- # Polinomio de interpolación
 import matplotlib.pyplot as plt
 import numpy as np
-def createMatrix(m,n,v):
+def createMatriz(m,n,v):
     C = []
     for i in range(m):
-        C.append([v]*n)
+        C.append([])
+        for j in range(n):
+            C[i].append(v)
+
     return C
 
-def getDimensions(A):
+def getDimensiones(A):
     return (len(A),len(A[0]))
 
-def copyMatrix(B):
-    m,n = getDimensions(B)
-    A = createMatrix(m,n,0)
-    for i in range(m):
-        for j in range(n):
-            A[i][j] = B[i][j]
-    return A
-
-def sumaMatrix(A,B):
-    Am,An = getDimensions(A)
-    Bm,Bn = getDimensions(B)
+def sumMatrices(A,B):
+    Am,An = getDimensiones(A)
+    Bm,Bn = getDimensiones(B)
     if Am != Bm or An != Bn:
-        print("Error las dimensiones deben ser iguales")
+        print("Las dimensiones son diferentes")
         return []
-    C = createMatrix(Am,An,0)
+    C = createMatriz(Am,An,0)
     for i in range(Am):
         for j in range(An):
             C[i][j] = A[i][j] + B[i][j]
     return C
 
-def restaMatrix(A,B):
-    Am,An = getDimensions(A)
-    Bm,Bn = getDimensions(B)
+def restaMatrices(A,B):
+    Am,An = getDimensiones(A)
+    Bm,Bn = getDimensiones(B)
     if Am != Bm or An != Bn:
-        print("Error las dimensiones deben ser iguales")
+        print("Las dimensiones son diferentes")
         return []
-    C = createMatrix(Am,An,0)
+    C = createMatriz(Am,An,0)
     for i in range(Am):
         for j in range(An):
             C[i][j] = A[i][j] - B[i][j]
     return C
 
-def multMatrix(A,B):
-    Am,An = getDimensions(A)
-    Bm,Bn = getDimensions(B)
+def mulMatrices(A,B):
+    Am,An = getDimensiones(A)
+    Bm,Bn = getDimensiones(B)
     if An != Bm:
-        print("Error las dimensiones deben ser conformable")
+        print("Las matrices no son conformables")
         return []
-    C = createMatrix(Am,Bn,0)
+    C = createMatriz(Am,Bn,0)
     for i in range(Am):
         for j in range(Bn):
             for k in range(An):
-                    C[i][j] += A[i][k] * B[k][j]
+                C[i][j] += A[i][k] * B[k][j]
     return C
-    
-def getAdyacente(A,r,c):
-    Am,An = getDimensions(A)
-    C = createMatrix(Am-1,An-1,0)
-    for i in range(Am):
+def getMenorMatriz(A,r,c):
+    m,n = getDimensiones(A)
+    C = createMatriz(m-1,n-1,0)
+    for i in range(m):
         if i == r:
             continue
-        for j in range(An):
+        for j in range(n):
             if j == c:
                 continue
-            ci = 0
-            cj = 0
-            if(i < r):
-                ci = i
-            else:
-                ci = i - 1
-            if(j < c):
-                cj = j
-            else:
-                cj = j - 1
-            C[ci][cj] = A[i][j]
+            Ci = i
+            if i > r:
+                Ci = i - 1
+            Cj = j
+            if j > c:
+                Cj = j -1
+            C[Ci][Cj] = A[i][j]
     return C
 
-def detMatrix(A):
-    m,n = getDimensions(A)
+def detMatriz(A):
+    m,n = getDimensiones(A)
     if m != n:
         print("La matriz no es cuadrada")
-        return []
+        return -1
     if m == 1:
-        return A[0][0]
+        return m
     if m == 2:
-        return A[0][0]*A[1][1] - A[1][0]*A[0][1]
+        return  A[0][0]*A[1][1] - A[0][1]*A[1][0]
     det = 0
-    for j in range(m):
-        det += ((-1)**j)*A[0][j]*detMatrix(getAdyacente(A,0,j))
+    for j in range(n):
+        det += (-1)**(j)*A[0][j]*detMatriz(getMenorMatriz(A,0,j))
     return det
 
+def getMatrizAdyacente(A):
+    m,n = getDimensiones(A)
+    C = createMatriz(m,n,0)
+    for i in range(m):
+        for j in range(n):
+            C[i][j] = (-1)**(i+j)*detMatriz(getMenorMatriz(A,i,j))
+    return C
 
 def getMatrizTranspuesta(A):
-    m,n = getDimensions(A)
-    C = createMatrix(n,m,0)
+    m,n = getDimensiones(A)
+    C = createMatriz(n,m,0)
     for i in range(m):
         for j in range(n):
             C[j][i] = A[i][j]
     return C
 
-def getMatrizAdjunta(A):
-    m,n = getDimensions(A)
-    if m != n:
-        print("La matriz no es cuadrada")
-        return []
-    C = createMatrix(m,n,0)
-    for i in range(m):
-        for j in range(n):
-            C[i][j] = ((-1)**(i+j))*detMatrix(getAdyacente(A,i,j))
-    return C
-
 def getMatrizInversa(A):
-    m,n = getDimensions(A)
-    if m != n:
-        print("La matriz no es cuadrada")
-        return []
-    detA = detMatrix(A)
+    detA = detMatriz(A)
     if detA == 0:
         print("La matriz no tiene inversa")
-        return []
-    At = getMatrizTranspuesta(A)
-    adjA = getMatrizAdjunta(At)
-    invDetA = 1/detA
-    C = createMatrix(m,n,0)
+        return 0
+    At =  getMatrizTranspuesta(A)
+    adyAt =  getMatrizAdyacente(At)
+    m,n = getDimensiones(A)
+    C = createMatriz(m,n,0)
     for i in range(m):
         for j in range(n):
-            C[i][j] = invDetA * adjA[i][j]
+            C[i][j] = (1/detA)*adyAt[i][j]
     return C
+# 77,68,70,59,58,64,72,57,63]
+# ,425,346,267,368,295,487,481,374,252]
+x = [67,52,56,66,65,80]
+y = createMatriz(1,len(x),0)
+y[0] = [481,292,357,396,345,469]
+C = getMatrizTranspuesta(y)
+A = createMatriz(len(x),len(x), 0)
+for i in range(len(x)):
+    for j in range(len(x)):
+        A[i][j] = x[i]**(j)
+print("A",A)
+invA = getMatrizInversa(A)
+coef = mulMatrices(invA,C)
+print("coef",coef)
 
-def evalPolinomio(coef,x):
+plt.plot(x,C,'rx')
+
+x2 = np.linspace(50, 81, 100)
+
+def evalua(x,coef):
     y = []
-    coef = np.asarray(coef)
     for i in range(len(x)):
         y.append(0)
-        for c in range(len(coef)):
-            y[i] += (x[i]**c) * coef[c]
+        for j in range(len(coef)):
+            y[i] += coef[j][0] * x[i] **(j)
     return y
 
-
-x = [10,20,30]
-y = createMatrix(3,1,0)
-y[0] = [0.1763]
-y[1] = [0.3640]
-y[2] = [0.5774]
-n = 3
-A = createMatrix(n,n,0)
-for i in range(n):
-    for j in range(n):
-        A[i][j] = x[i]**j
-print(A)
-
-invA = getMatrizInversa(A)
-B = multMatrix(invA,y)
-
-print(B)
-plt.plot(x,y,'ro')
-x2 = np.linspace(10, 30, 100)
-y2 = evalPolinomio(B, x2)
+y2 = evalua(x2,coef)
 plt.plot(x2,y2)
 plt.show()
