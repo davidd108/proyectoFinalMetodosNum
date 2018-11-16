cd1edff5bd864ab0bedfb353d0b4c813b4905f76
davidd108 <41306199+davidd108@users.noreply.github.com>
2018-11-15 19:40:15 -0600

    Update codigo.py

--- metodoDeLagrange/codigo.py
+++ metodoDeLagrange/codigo.py
@@ -1,31 +1,31 @@
+# Ejemplo lagrange
 from sympy import *
 import matplotlib.pyplot as plt
 import numpy as np
-# 77,68,70,59,58,64,72,57,63]
-# ,425,346,267,368,295,487,481,374,252]
-x = [67,52,56,66,65,80]
-y = [481,292,357,396,345,469]
+
+x = [0,0.33,0.66,1]
+y = [1,1.391,1.935,2.718]
 
 pL = ''
-for k in range(len(y)):
-    pL += str(y[k]) +'*'
+for k in range(len(x)):
+    pL +=  str(y[k]) + '* ('
     Lxk = 1
-    for i in range(len(x)):
-        if (i == k):
+    for j in range(len(x)):
+        if (j == k):
             continue
-        pL += '(x - %f)*'%(x[i])
-        Lxk *= (x[k]-x[i])
-    pL = pL[:-1]
-    pL += '/%f+'%(Lxk)
+        pL += '(x - %f)*'%(x[j])
+        Lxk *= x[k] - x[j]
+    pL = pL[:-1] + '/%f) +'%(Lxk) 
 pL = pL[:-1]
 
 expr = sympify(pL)
-#expr = expand(expr)
-print(expand(expr))
-plt.plot(x,y,'go')
+expr = expand(expr)
+print(expr)
 
-x2 = np.linspace(52,80,100)
+plt.plot(x,y,'ro')
+x2 = np.linspace(0,1,100)
+y2 = []
 x = symbols('x')
-y2 = [expr.subs(x,xi) for xi in x2]
+for i in range(len(x2)):
+    y2.append(expr.subs(x,x2[i]))
 plt.plot(x2,y2)
-plt.grid()
