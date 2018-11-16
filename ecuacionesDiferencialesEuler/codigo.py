8fc60a0f878a2754aa89dca94a755c9e0ce4ffda
davidd108 <41306199+davidd108@users.noreply.github.com>
2018-11-16 10:46:27 -0600

    Update codigo.py

--- ecuacionesDiferencialesEuler/codigo.py
+++ ecuacionesDiferencialesEuler/codigo.py
@@ -1,26 +1,27 @@
-# EDO dx/dy = e**x condiciones iniciales y(0) = 1
-# El valor analítico es e**x
-import numpy as np
+# Metodo de Euler
+# y' - e^x = 0 y(0) =1  y(5) = ?
+# dy/dx = e^x
 import math
+import numpy as np
 import matplotlib.pyplot as plt
-n = 40
-a = 0
-b = 5  # Este es propuesto
-h = abs(a-b)/(n -1)
-x = np.linspace(a,b,n)
-y0 = 1
 
-def fprima(x,y):
-    return math.exp(x)
+def f(x,y):
+    return math.sin(x) + math.log(y)
 
-def euler(h,x,y0):
+def euler(y0,x,h,f):
     y = []
     y.append(y0)
     for i in range(1,len(x)):
-        y.append(y[i-1] + h*fprima(x[i-1],y[i-1]))
+        y.append( y[i-1] + h*f(x[i-1],y[i-1]) )
     return y
 
-y = euler(h,x,y0)
+n = 20
+a = 0.13
+b = 0.14
+h = abs(a-b)/(n-1)
+x = np.linspace(a,b,n)
+y = euler(1,x,h,f)
 
-plt.plot(x,y,'b')
-plt.plot(x,[math.exp(xi) for xi in x],'r')
+plt.plot(x,y,'g')
+#plt.plot(x,[math.exp(xi) for xi in x],'b')
+plt.grid()
